312f5f6550419886c2c3b31220d3fa4cbc74d35c
ZhengYu, Xu <zen-xu@outlook.com>
2025-07-10 22:20:29 +0800

    impl summary stats

--- pytest_modern/terminal.py
+++ pytest_modern/terminal.py
@@ -5,17 +5,17 @@ import rich.console
 import rich.rule
 import rich.theme
 
+from _pytest import terminal
 from _pytest import timing
-from _pytest.terminal import TerminalReporter
 from rich.panel import Panel
 
 from .header import generate_header_group
 
 
-class ModernTerminalReporter(TerminalReporter):  # type: ignore[final-class]
-    def __init__(self, reporter: TerminalReporter) -> None:
+class ModernTerminalReporter(terminal.TerminalReporter):  # type: ignore[final-class]
+    def __init__(self, reporter: terminal.TerminalReporter) -> None:
         super().__init__(reporter.config)
-        self.console = rich.console.Console()
+        self.console = rich.console.Console(highlight=False)
 
     @pytest.hookimpl(trylast=True)
     def pytest_sessionstart(self, session: pytest.Session) -> None:
@@ -30,6 +30,25 @@ class ModernTerminalReporter(TerminalReporter):  # type: ignore[final-class]
     def summary_stats(self) -> None:
         if self.verbosity < -1:
             return
-        super().summary_stats()
         self.console.print("────────────")
-        self.console.print("     [green]Summary[/green]")
+        session_duration = format_node_duration(self._session_start.elapsed().seconds)
+        stat_counts = {
+            stat_type: len(self._get_reports_to_display(stat_type))
+            for stat_type in self._known_types or []
+        }
+        stats = ", ".join(
+            f"[bold]{count}[/] [bold {terminal._color_for_type.get(stat_type, terminal._color_for_type_default)}]{stat_type}[/]"
+            for stat_type, count in stat_counts.items()
+            if count > 0
+        )
+
+        self.console.print(
+            f"     [green]Summary[/] [{session_duration}] [bold]{sum(stat_counts.values())}[/] tests run: {stats}"
+        )
+
+
+def format_node_duration(seconds: float) -> str:
+    duration = terminal.format_node_duration(seconds).strip()
+    if len(duration) < 10:
+        return f"{duration:>10s}"
+    return duration
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,27 @@
-def test_example():
+import pytest
+
+
+def test_passed():
     a = 1
     b = 2
     assert a + b == 3
+
+
+def test_failed():
+    a = 1
+    b = 2
+    assert a + b == 2
+
+
+@pytest.mark.skip(reason="skipped")
+def test_skipped():
+    a = 1
+    b = 2
+    assert a + b == 2
+
+
+@pytest.mark.xfail(reason="expected to fail but passed")
+def test_xpassed():
+    a = 1
+    b = 2
+    assert a + b == 2
